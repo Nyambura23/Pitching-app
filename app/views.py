f3b437afa5ef620c9722bcd24c29dee249cfa9d1
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-11 02:37:41 +0300

    add a new pitch route

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Pitch Perfect'
+    return render_template('index.html',message=message)
+
+@app.route('/pitches')
+def pitches():
+
+    '''
+    View pitches page function that returns the pitches details page and its data
+    '''
+    return render_template('pitches.html')
